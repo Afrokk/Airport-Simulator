cf722f79d18fe60dc4bc6cf775fb61c8d1e725de
Afrasiyab Khan <74003001+Afrokk@users.noreply.github.com>
2022-09-07 19:23:48 -0400

    Tester Class for the Airport Simulator.

--- AirportTester.java
+++ AirportTester.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
-/**
-   A class to test the Airport class.
-*/
+
 public class AirportTester {
    public static void main(String[] args) {
       Airport simulator = new Airport();
